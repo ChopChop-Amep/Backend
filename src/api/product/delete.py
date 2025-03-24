029feffc7b06723b6be07ad217196840fc71e64d
Slushee <slushee@duck.com>
2025-03-24 19:55:09 +0100

    Apply suggested changes

--- src/api/product/delete.py
+++ src/api/product/delete.py
@@ -1,33 +1,46 @@
-from fastapi import APIRouter, Depends, HTTPException
-from psycopg import sql
 from uuid import UUID
 
-from auth import get_current_user, TokenData
+from fastapi import APIRouter, Depends, HTTPException
+from psycopg import sql
+
+from auth import authenticate, User
 from database import get_db_connection
 
 router = APIRouter()
 
 
-@router.delete(
-    "/product/delete", description="Delete a product from the database"
-)
-async def delete_product(
-    product_id: UUID, current_user: TokenData = Depends(get_current_user)
-):
+@router.delete("/product/delete/{product_id}", description="Delete a product")
+async def delete_product(product_id: UUID, user: User = Depends(authenticate)):
     try:
         conn = get_db_connection()
         with conn:
             with conn.cursor() as cursor:
                 cursor.execute(
-                    sql.SQL("DELETE FROM products WHERE id = %s RETURNING id"),
-                    (product_id,)
+                    sql.SQL(
+                        "DELETE FROM VerifiedProduct WHERE id = %s AND vp_owner = %s RETURNING id"
+                    ),
+                    (product_id, user.id),
                 )
                 deleted_id = cursor.fetchone()
+
+                # If it's not a VerifiedProduct, it's probably a SecondHandProduct. Attempt to delete from that table
                 if not deleted_id:
-                    raise HTTPException(
-                        status_code=404, detail="Product not found")
-        return {"message": "Product deleted", "product_id": product_id}
+                    cursor.execute(
+                        sql.SQL(
+                            "DELETE FROM SecondHandProduct WHERE id = %s AND vp_owner = %s RETURNING id"
+                        ),
+                        (product_id, user.id),
+                    )
+                    deleted_id = cursor.fetchone()
+
+                # If it wasn't a SecondHandProduct either, it's either not an existing product or the user doesn't have permission to delete it. Return an error.
+                if not deleted_id:
+                    raise HTTPException(status_code=404, detail="Product not found")
+
+        return {"product_id": product_id}
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
     finally:
         conn.close()
