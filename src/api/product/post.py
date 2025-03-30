d4a638b1c21db71c9c3133c4300aa1538f299cec
Slushee <55996847+Slushee-a@users.noreply.github.com>
2025-03-30 16:46:37 +0200

    Feat - User types
    closes #7 closes #13

--- src/api/product/post.py
+++ src/api/product/post.py
@@ -1,11 +1,9 @@
-from uuid import UUID
-
 from fastapi import APIRouter, Depends, HTTPException
-from psycopg import sql
 
 from auth import authenticate
 from database import get_db_connection
-from model.product import NewProduct, ProductType
+from model.product.product import NewProduct
+from model.user import User
 
 router = APIRouter()
 
@@ -14,7 +12,7 @@ router = APIRouter()
     "/product",
     description="Create a new product",
 )
-async def post_product(product: NewProduct, user_id: UUID = Depends(authenticate)):
+async def post_product(product: NewProduct, user: User = Depends(authenticate)):
     try:
         conn = get_db_connection()
         with conn:
@@ -24,70 +22,8 @@ async def post_product(product: NewProduct, user_id: UUID = Depends(authenticate
                 )
 
                 try:
-                    insert_product_query = sql.SQL("""
-                        INSERT INTO chopchop.product_id DEFAULT VALUES
-                        RETURNING product_id
-                    """)
-                    cursor.execute(insert_product_query)
-                    product_id = cursor.fetchone()[0]
-
-                    match product.type_:
-                        case ProductType.VERIFIED:
-                            insert_verified_query = sql.SQL("""
-                                INSERT INTO chopchop.verified_product (
-                                    vp_id, 
-                                    vp_owner, 
-                                    vp_sku, 
-                                    vp_name, 
-                                    vp_description, 
-                                    vp_stock, 
-                                    vp_price, 
-                                    vp_image, 
-                                    vp_category
-                                )
-                                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-                            """)
-                            cursor.execute(
-                                insert_verified_query,
-                                (
-                                    product_id,
-                                    user_id,
-                                    product.sku,
-                                    product.name,
-                                    product.description,
-                                    product.stock,
-                                    product.price,
-                                    product.image,
-                                    product.category.value,
-                                ),
-                            )
-
-                        case ProductType.SECONDHAND:
-                            insert_secondhand_query = sql.SQL("""
-                                INSERT INTO chopchop.secondhand_product (
-                                    sp_id, 
-                                    sp_owner, 
-                                    sp_name, 
-                                    sp_description, 
-                                    sp_price, 
-                                    sp_image, 
-                                    sp_category
-                                )
-                                VALUES (%s, %s, %s, %s, %s, %s, %s)
-                            """)
-                            cursor.execute(
-                                insert_secondhand_query,
-                                (
-                                    product_id,
-                                    user_id,
-                                    product.name,
-                                    product.description,
-                                    product.price,
-                                    product.image,
-                                    product.category.value,
-                                ),
-                            )
-
+                    product = product.into_product()
+                    product_id = product.insert(cursor, user)
                     conn.commit()  # Cometre la transacció si tot ha anat bé
                     return product_id
 
--- src/auth.py
+++ src/auth.py
@@ -5,6 +5,8 @@ import jwt
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 
+from model.user import User, Particular, Professional, Enterprise, Admin
+
 SECRET_KEY = os.getenv("SECRET_KEY", "Secret key missing!!")
 ALGORITHM = "HS256"
 
@@ -14,7 +16,40 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 def verify_jwt_token(token: str):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        return UUID(payload.get("sub"))
+
+        type_ = User.Type(payload.get("type"))
+
+        match type_:
+            case User.Type.PARTICULAR:
+                return Particular(
+                    id_=UUID(payload.get("sub")),
+                    name=payload.get("name"),
+                    surname=payload.get("surname"),
+                )
+
+            case User.Type.PROFESSIONAL:
+                return Professional(
+                    id_=UUID(payload.get("sub")),
+                    name=payload.get("name"),
+                    surname=payload.get("surname"),
+                    nif=payload.get("nif"),
+                )
+
+            case User.Type.ENTERPRISE:
+                return Enterprise(
+                    id_=UUID(payload.get("sub")),
+                    name=payload.get("name"),
+                    surname=payload.get("surname"),
+                    nif=payload.get("nif"),
+                )
+
+            case User.Type.ADMIN:
+                return Admin(
+                    id_=UUID(payload.get("sub")),
+                    name=payload.get("name"),
+                    surname=payload.get("surname"),
+                )
+
     except jwt.PyJWTError:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
