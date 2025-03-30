cc6735a81fbbcd2346090761996ed4db00c4ca61
Slushee <slushee@duck.com>
2025-03-30 11:41:04 +0200

    I forgot we were doing OOP

--- src/model/user.py
+++ src/model/user.py
@@ -6,17 +6,22 @@ from pydantic import BaseModel, Field
 
 
 class User(BaseModel):
-    class Type(Enum):
-        PARTICULAR = "particular"
-        PROFESSIONAL = "professional"
-        ENTERPRISE = "enterprise"
-        ADMIN = "admin"
-
-    VERIFIED_TYPES: ClassVar[tuple[Type, ...]] = (Type.PROFESSIONAL, Type.ENTERPRISE)
-    SECONDHAND_TYPES: ClassVar[tuple[Type, ...]] = (Type.PROFESSIONAL, Type.PARTICULAR)
-
     id_: UUID = Field(..., alias="id")
     name: str
     surname: str
-    type_: Type = Field(..., alias="type")
-    nif: Optional[str]  # Only PROFESSIONAL and ENTERPRISE have a `nif`
+
+class Particular(User):
+    def whatever():
+        print("hi")
+
+class Professional(User):
+    def whatever():
+        print("hi")
+
+class Enterprise(User):
+    def whatever():
+        print("hi")
+
+class Admin(User):
+    def whatever():
+        print("hi")
