9805702c799036ee0c44787961eca61d19eb6bf5
Slushee <slushee@duck.com>
2025-03-30 12:29:12 +0200

    Much better

--- src/model/product.py
+++ src/model/product.py
@@ -4,6 +4,7 @@ from typing import Optional
 
 from pydantic import BaseModel, Field
 from psycopg import Cursor, sql
+from fastapi import HTTPException
 
 from model import User, Particular, Professional, Enterprise
 
@@ -45,8 +46,54 @@ class Product(BaseModel):
     image: str
     category: Category
 
+    def fetch(self, cursor: Cursor, product_id: UUID):
+        query = """
+            SELECT 
+                CASE 
+                    WHEN vp.vp_id IS NOT NULL THEN 'verified_product'
+                    WHEN sp.sp_id IS NOT NULL THEN 'secondhand_product'
+                    ELSE 'not found'
+                    END AS product_type
+                FROM 
+                    chopchop.product_id p
+                LEFT JOIN 
+                    chopchop.verified_product vp ON p.product_id = vp.vp_id
+                LEFT JOIN 
+                    chopchop.secondhand_product sp ON p.product_id = sp.sp_id
+                WHERE 
+                    p.product_id = %s;
+        """
+        cursor.execute(query, (product_id,))
+
+        match cursor.fetchone():
+            case "verified_product":
+                return VerifiedProduct.fetch(cursor, product_id)
+            case "secondhand_product":
+                return SecondhandProduct.fetch(cursor, product_id)
+            case None:
+                raise HTTPException(status_code=404, detail=str("Product not found"))
+
 
 class SecondhandProduct(Product):
+    def fetch(self, cursor: Cursor, product_id: UUID):
+        query_secondhand = sql.SQL(
+            """
+            SELECT sp_id, sp_owner, sp_name, sp_description, sp_price, sp_image, sp_category
+            FROM chopchop.secondhand_product
+            WHERE sp_id = %s;
+            """
+        )
+        cursor.execute(query_secondhand, (product_id,))
+        response = cursor.fetchone()
+
+        self.id_ = (UUID(response[0]),)
+        self.owner = (UUID(response[1]),)
+        self.name = (response[2],)
+        self.description = (response[3],)
+        self.price = (float(response[4]),)
+        self.image = (response[5],)
+        self.category = (Category(response[6]),)
+
     def insert(self, cursor: Cursor, user: User):
         if not (isinstance(user, Particular) or isinstance(user, Professional)):
             raise Exception("This user is not allowed to post verified products")
@@ -90,6 +137,28 @@ class VerifiedProduct(Product):
     sku: str
     stock: int
     sold: int
+
+    def fetch(self, cursor: Cursor, product_id: UUID):
+        query_verified = sql.SQL(
+            """
+            SELECT vp_id, vp_owner, vp_sku, vp_name, vp_description, vp_stock, vp_price, vp_image, vp_category, vp_sold
+            FROM chopchop.verified_product
+            WHERE vp_id = %s;
+            """
+        )
+        cursor.execute(query_verified, (product_id,))
+        response = cursor.fetchone()
+
+        self.id_ = (UUID(response[0]),)
+        self.owner = (UUID(response[1]),)
+        self.sku = (response[2],)
+        self.name = (response[3],)
+        self.description = (response[4],)
+        self.stock = (int(response[5]),)
+        self.price = (float(response[6]),)
+        self.image = (response[7],)
+        self.category = (Category(response[8]),)
+        self.sold = (response[9],)
 
     def insert(self, cursor: Cursor, user: User):
         if not (isinstance(user, Professional) or isinstance(user, Enterprise)):
