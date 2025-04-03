24112f6b6acf07c91e82dc6e2d2a28650b35c68f
m4ik4p <miquel.lleida@estudiantat.upc.edu>
2025-04-03 10:02:15 +0200

    Feat - Get products

---
+++ src/api/products/get.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, HTTPException
+
+from database import get_db_connection
+from model.product.product import Product
+
+router = APIRouter()
+
+
+@router.get(
+    "/products",
+    description="Get basic information of all products",
+)
+async def get_products():
+    try:
+        conn = get_db_connection()
+        with conn:
+            with conn.cursor() as cursor:
+                return Product.get_products(cursor)
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    finally:
+        conn.close()
