ec67d5a1c0a6f60ce8dc395d27bb740a0f03a8df
M4IK4P <miquel.lleida@estudiantat.upc.edu>
2025-05-17 22:42:11 +0200

    Correcció format

--- src/model/product/product.py
+++ src/model/product/product.py
@@ -80,7 +80,8 @@ class Product(BaseModel):
                 return SecondhandProduct(_id=product_id)
 
             case "not found":
-                raise HTTPException(status_code=404, detail="Product not found")
+                raise HTTPException(
+                    status_code=404, detail="Product not found")
 
             case _:
                 raise HTTPException(
