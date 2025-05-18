6f0a48840a405239ffaeaa5c0d8972e067207950
M4IK4P <miquel.lleida@estudiantat.upc.edu>
2025-05-18 12:04:05 +0200

    Sobreescriptura flake erroni

--- flake.nix
+++ flake.nix
@@ -1,36 +1,37 @@
 {
-  description = "ChopChop Backend";
+    description = "ChopChop Backend";
 
-  inputs = {
-    nixpkgs.url     = "github:NixOS/nixpkgs/nixos-24.11";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+    inputs = {
+        nixpkgs.url     = "github:NixOS/nixpkgs/nixos-24.11";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+    outputs = { self, nixpkgs, flake-utils, ... }:
+        flake-utils.lib.eachDefaultSystem (system:
+            let
+                pkgs = import nixpkgs { inherit system; };
+                
+                python = with pkgs; [ python312Full ];
 
-        python = with pkgs; [ python312Full ];
+                dependencies = with pkgs.python312Packages; [
+                    fastapi
+                    psycopg
+                    pydantic
+                    pyjwt
+                ];
 
-        dependencies = with pkgs.python312Packages; [
-          fastapi
-          psycopg
-          pydantic
-          pyjwt
-        ];
+                dev_tools = with pkgs.python312Packages; [
+                    bandit
 
-        dev_tools = with pkgs.python312Packages; [
-          bandit
-          python-lsp-server
-          python-lsp-ruff
-          autopep8
-        ];
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = python ++ dependencies ++ dev_tools;
-          stdenv = pkgs.clangStdenv;
-        };
-      }
-    );
+                    python-lsp-server
+                    python-lsp-ruff
+                    autopep8
+                ];
+
+            in { 
+                devShells.default = pkgs.mkShell {
+                    buildInputs = python ++ dependencies ++ dev_tools;
+                };
+            }
+        );
 }
